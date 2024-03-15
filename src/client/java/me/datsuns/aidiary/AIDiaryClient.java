6a2fc33d3aa3cf45e775c3d3ed8e426816a46b1b
datsuns <the.skeleton7@gmail.com>
2024-03-15 22:40:19 +0900

    replace to template generated

--- src/client/java/me/datsuns/aidiary/AIDiaryClient.java
+++ src/client/java/me/datsuns/aidiary/AIDiaryClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package me.datsuns.aidiary;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class AIDiaryClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
