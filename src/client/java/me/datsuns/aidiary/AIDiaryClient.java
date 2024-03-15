6d243bfca11bc9040230dd0ed3b4cf54dc41f319
datsuns <the.skeleton7@gmail.com>
2024-03-15 23:22:37 +0900

    code formatted by IntteliJ

--- src/client/java/me/datsuns/aidiary/AIDiaryClient.java
+++ src/client/java/me/datsuns/aidiary/AIDiaryClient.java
@@ -3,8 +3,8 @@ package me.datsuns.aidiary;
 import net.fabricmc.api.ClientModInitializer;
 
 public class AIDiaryClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
 }
