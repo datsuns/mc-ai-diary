2e7b6c4bfb94ff01d2c95f2dc626821fdade7682
datsuns <the.skeleton7@gmail.com>
2024-03-16 23:48:42 +0900

    fix typo and add dependency

--- src/client/java/me/datsuns/aidiary/Diary.java
+++ src/client/java/me/datsuns/aidiary/Diary.java
@@ -10,10 +10,8 @@ import net.minecraft.text.Text;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.IOException;
 import java.io.InputStream;
