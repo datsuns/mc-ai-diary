2df60ffb53c124212e6a38044615eb759f8182ad
datsuns <the.skeleton7@gmail.com>
2024-03-16 15:49:52 +0900

    imple: diary generation procedure

--- src/client/java/me/datsuns/aidiary/Diary.java
+++ src/client/java/me/datsuns/aidiary/Diary.java
@@ -1,44 +1,63 @@
 package me.datsuns.aidiary;
 
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.integrated.IntegratedServer;
 
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.CompletableFuture;
 
 public class Diary {
-    public String FileName;
-    public String SaveDir;
-    public Path DiaryPath;
-    public File DiaryFile;
+    public GenerationState State;
+    Diary() {
+        this.State = GenerationState.Idle;
+    }
 
-    Diary(String directory, String filename) {
-        this.FileName = filename;
-        this.SaveDir = "";
-        this.DiaryPath = null;
-        this.DiaryFile = null;
-        if( directory.equalsIgnoreCase("")){
-            AIDiaryClient.LOGGER.info("directory is not fixed. skip");
+    public void onClientTick(MinecraftClient client) {
+        if( this.State != GenerationState.Completed ){
             return;
         }
-        this.SaveDir = directory;
-        this.DiaryPath = Paths.get(this.SaveDir).resolve(this.FileName);
-        this.DiaryFile = this.DiaryPath.toFile();
-        AIDiaryClient.LOGGER.info("diary path: {}", this.DiaryPath.toAbsolutePath().toString());
+        AIDiaryClient.LOGGER.info("diary generated");
+        this.State = GenerationState.Idle;
     }
 
-    public void notifySaveDirectoryFixed(String directory) {
-        this.SaveDir = directory;
-        this.DiaryPath = Paths.get(this.SaveDir).resolve(this.FileName);
-        this.DiaryFile = this.DiaryPath.toFile();
-        AIDiaryClient.LOGGER.info("diary path: {}", this.DiaryPath.toAbsolutePath().toString());
-    }
-
-    public void onSave() {
-        if( this.DiaryFile == null ) {
-            AIDiaryClient.LOGGER.info("save target is not fixed");
-            return;
+    public void onSave(MinecraftClient client) {
+        if( this.State != GenerationState.Idle ){
+            AIDiaryClient.LOGGER.info("now on busy. skip.");
         }
         AIDiaryClient.LOGGER.info("save diary");
+        IntegratedServer s = client.getServer();
+        if (s == null) {
+            return;
+        }
+        ServerCommandSource src = s.getCommandSource();
+        CommandManager cm = s.getCommandManager();
+        String cmd = String.format("say %s", "hello");
+        cm.executeWithPrefix(src, cmd);
+        this.State = GenerationState.Generating;
+        CompletableFuture.runAsync( () -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            AIDiaryClient.LOGGER.info("done");
+            this.State = GenerationState.Completed;
+        });
+    }
+
+    public enum GenerationState {
+        Idle(0),
+        Generating(1),
+        Completed(2);
+
+        private final int n;
+        GenerationState(int i){
+            this.n = i;
+        }
     }
 }
