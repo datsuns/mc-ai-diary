f2cff5a963217d2c703a001a1b6040f7275763d9
datsuns <the.skeleton7@gmail.com>
2024-03-16 12:55:40 +0900

    add Stats class to store player stats

--- src/client/java/me/datsuns/aidiary/AIDiaryClient.java
+++ src/client/java/me/datsuns/aidiary/AIDiaryClient.java
@@ -1,10 +1,16 @@
 package me.datsuns.aidiary;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AIDiaryClient implements ClientModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("aidiary");
+    public Stats Stats;
+
     @Override
     public void onInitializeClient() {
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        this.Stats = new Stats();
     }
 }
