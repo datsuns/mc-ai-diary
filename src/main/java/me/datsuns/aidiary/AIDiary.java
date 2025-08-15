00e1457a9eb605767c166058eb4e0a2fdb3a1152
datsuns <the.skeleton7@gmail.com>
2025-08-15 14:11:12 +0900

    replace to template generated

--- src/main/java/me/datsuns/aidiary/AIDiary.java
+++ src/main/java/me/datsuns/aidiary/AIDiary.java
@@ -1,17 +1,15 @@
-package com.example;
+package me.datsuns.aidiary;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
-
+public class AIDiary implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LoggerFactory.getLogger("ai-diary");
 
 	@Override
 	public void onInitialize() {
